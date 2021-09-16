956cebb9267b1736d00104e40703f5a44914364c
M F M Fazrin <mfmfazrin1986@gmail.com>
2021-09-16 15:23:05 +0530

    Praesent scelerisque, mi sed ultrices condimentum,

--- Mango.Services.Identity/SD.cs
+++ Mango.Services.Identity/SD.cs
@@ -1,9 +1,6 @@
 ﻿using Duende.IdentityServer;
 using Duende.IdentityServer.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Mango.Services.Identity
 {
